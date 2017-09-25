60f05d741cf50930c0ee21a0db35de7946b084a1
DennySPB <dennyspb@gmail.com>
2017-09-25 15:12:22 +0300

    Fix mismerge after ColorEngine merge

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1420,7 +1420,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     QSContainer qsContainer = (QSContainer) v.findViewById(
                             R.id.quick_settings_container);
                     qsContainer.setHost(qsh);
-                    mBrightnessMirrorController.onDensityOrFontScaleChanged();
+//                    mBrightnessMirrorController.onDensityOrFontScaleChanged();
                     mQSPanel = qsContainer.getQsPanel();
                     mKeyguardStatusBar.setQSPanel(mQSPanel);
                     mHeader = qsContainer.getHeader();
